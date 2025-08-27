4ea294264ffe48cdce89ac5406ed270465520db4
Mukesh <mukesh7rathore77@gmail.com>
2025-08-27 09:19:12 +0530

    Completely deleted Middleware folder

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
+const auth = require("../Middleware/auth");
 const Job = require("../models/Job");
 
 //  Add Job Route
